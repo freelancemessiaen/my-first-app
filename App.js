438228899efa54d394a69b153e7252c767dba4ed
freelancemessiaen <153985208+freelancemessiaen@users.noreply.github.com>
2024-02-22 15:49:31 +0100

    flex box
    flex box

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { FlexDemo } from "./components/FlexDemo";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ backgroundColor: "grey", flex: 6 }}>
+        <FlexDemo />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Human/Human.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native"
+import { car, drive } from "./Animal"
+export function Human () {
+    return  (
+    <View>        
+        <Text>
+            Je suis un humain
+        </Text>
+    </View>
+
+    )
+}
